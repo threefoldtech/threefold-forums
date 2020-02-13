b33fd76e4014fa134cb991d16657355f84dd1a4a
root <root@c83b155a1e4b>
2020-02-13 13:22:44 +0000

    adding crisp

---
+++ app/assets/crisp.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "6a0cfee3-c01c-4dc0-8d02-d4849991b3bf";
+(function() {
+    d = document;
+    s = d.createElement("script");
+    s.src = "https://client.crisp.chat/l.js";
+    s.async = 1;
+    d.getElementsByTagName("head")[0].appendChild(s);
+})();
